eaab12df624b8934d2893a41dafb5889ee13226b
Ratin <ratinhaldar123@gmail.com>
2026-08-26 01:36:55 +0530

    JavaScript  series started

---
+++ JavaScript/Basic 01/Ep-01/script.js
@@ -0,0 +1,24 @@
+var a = "ratin";
+let b = "haldar";
+const c = "khatima";
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+a = "ratin Updated";
+console.log(a);
+b = "haldar updated";
+console.log(b);
+
+ratin = 7;
+ratin = 8;
+console.log(ratin);
+
+
+// console.table([a, b, c]);
+
+
+
+
+
