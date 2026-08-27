db1fc3018c363ec0b99d4f3c404eaf5b0a334b7c
Ratin <ratinhaldar123@gmail.com>
2026-08-28 00:13:41 +0530

    data type comparison

---
+++ JavaScript/Basic 01/Ep-04/script.js
@@ -0,0 +1,6 @@
+let num1 = 4;
+console.log(num1);
+
+let negativeNum = -num1;
+console.log(negativeNum)
+
---
+++ JavaScript/Basic 01/Ep-05/script.js
@@ -0,0 +1,68 @@
+// console.log("2" > 1);
+
+// 01 "10" + 5 ka output kya hoga aur iska datatype kya hoga?
+// 02 "20" - 5 ka output kya hoga aur iska datatype kya hoga?
+// 03 "10" * "2" ka output kya hoga?
+// 04 String(100) aur 100 + "" mein kya difference hai?
+// 05 "25" == 25 ka result kya hoga?
+// 06 "25" === 25 ka result kya hoga aur == se kya difference hai?
+
+// console.log(typeof ("10" + 5));
+// console.log(typeof ("10" - 5));
+// console.log(typeof ("10" * "2"));
+// console.log(typeof (100 + ""));
+
+
+
+// 07 "5" + 2 + 3 ka output kya hoga?
+// 08  5 + 2 + "3" ka output kya hoga?
+// 09 "10" - "2" + "5" ka output kya hoga?
+// 10 "10" + -"5" ka output kya hoga?
+// 11 Number("10px") ka output kya hoga?
+// 12 parseInt("10px") aur Number("10px") mein kya difference hoga?
+// 13 Boolean("false") ka result kya hoga? Kyun?
+// 14 String(null), String(undefined), String(true) aur String(false) ka output kya hoga?
+
+// console.log("10" - "2" + "5");
+// console.log(typeof ("10" + -"5"));
+
+// console.log(typeof (Number("10px")));
+
+// console.log(typeof (Number("10px")));
+// console.log(Boolean("false"));
+
+
+// 15 In sabka result predict karo:
+// 0 == false
+// 0 === false
+// "0" == false
+// "0" === false
+
+// 16 Inka output predict karo:
+// null == undefined
+// null === undefined
+// null == 0
+// undefined == 0
+
+// 17 "5" > 3 aur "5" > "30" ka result kya hoga ? Dono mein difference kyun hai ?
+
+// 18 "" == 0, "" === 0, false == 0, aur false === 0 ka result kya hoga ?
+
+// 19   Output predict karo:
+// let a = "10";
+// let b = 10;
+
+// console.log(a + b); 1010
+// console.log(a - b); 0
+// console.log(a == b); true
+// console.log(a === b); false
+
+
+// 20 console.log("10" + 5 - 2); 103
+// console.log("10" - 5 + "2"); 52
+// console.log(true + true); true
+// console.log(true + "true"); truetrue
+// console.log([] == false); true
+// console.log([] === false); false
+// console.log(null == 0); false
+// console.log(null === 0); false
