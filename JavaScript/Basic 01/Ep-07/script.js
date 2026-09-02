aee92ecd2576b242e398477fc33b8bd3c61cb403
Ratin <ratinhaldar123@gmail.com>
2026-09-03 01:29:37 +0530

    string done

---
+++ JavaScript/Basic 01/Ep-07/script.js
@@ -0,0 +1,29 @@
+const name = "Ratin Haldar";
+// const age = 24;
+
+// console.log(`my name is ${name} and I am ${age} years old.`);
+
+// const a = new String("Ratin Haldar");
+
+// console.log(a);
+
+// console.log(a.__proto__);
+
+// console.log(name.toUpperCase());
+
+// console.log(name.charAt(9));
+
+// console.log(name.indexOf("Haldar"));
+
+// console.log(name.substring(9, 20));
+
+// console.log(name.slice(0, 4));
+
+// let name1 = "   1 Ratin Haldar   ";
+// console.log(name1.length);
+// console.log(name1);
+// console.log(name1.trim());
+
+// console.log(name.replace(" ", "-"));
+
+console.log(name.includes("ratin"));
