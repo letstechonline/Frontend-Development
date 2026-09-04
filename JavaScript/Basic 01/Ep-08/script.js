e614df9dc9cab4ff922548392e31825f7e00b440
Ratin <ratinhaldar123@gmail.com>
2026-09-05 00:36:49 +0530

    numbers and math object

---
+++ JavaScript/Basic 01/Ep-08/script.js
@@ -0,0 +1,41 @@
+// ************ Number Object ************
+
+// let num = 400;
+// console.log(typeof num);
+
+// let balance = new Number(num);
+
+// console.log(typeof balance);
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+// const price = 99.9973;
+
+// console.log(price.toPrecision(3));
+
+// const price = 9999730000;
+// console.log(price.toLocaleString('en-IN'));
+
+// const price2 = 9999730000;
+// console.log(price2.toLocaleString('en-US'));
+
+// ************** Math Object ************
+
+// console.log(Math.PI)
+// console.log(Math.round(4.46));
+// console.log(Math.round(4.56));
+
+// const num = -4.56;
+// const result = Math.abs(num);
+
+// console.log(Math.floor(result));
+
+
+// let randomNumber = Math.random() * 10;
+
+// console.log(Math.round(randomNumber));
+
+
+
