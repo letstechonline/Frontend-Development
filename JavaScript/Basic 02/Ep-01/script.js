1c51c35b2f746685744163f9b75025e7d6b5d216
Ratin <ratinhaldar123@gmail.com>
2026-09-07 23:44:42 +0530

    array in javascript

---
+++ JavaScript/Basic 02/Ep-01/script.js
@@ -0,0 +1,39 @@
+// Array
+// const myArry = [1, 2, 3, 4, 5];
+// console.log(myArry);
+
+// const myHeros = ['Iron Man', 'Spider Man', 'Thor', 'Hulk', 'Captain America'];
+
+// console.log(myHeros.length);
+
+// let myNumbers = [1, 'ratin', 2, 3, 4, 5, 6];
+
+// console.log(myNumbers);
+
+// console.log(myNumbers.push(7));
+// console.log(myNumbers);
+
+// console.log(myNumbers);
+
+// console.log(myNumbers.pop());
+
+// console.log(myNumbers);
+
+// console.log(myNumbers.shift());
+
+// console.log(myNumbers.unshift(1));
+// console.log(myNumbers);
+
+// console.log(myNumbers.indexOf(2));
+
+// console.log(typeof myNumbers.join());
+
+// let mynum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// console.log(mynum);
+
+// console.log(mynum.slice(2, 5));
+// console.log(mynum);
+
+// console.log(mynum.splice(2, 5));
+// console.log(mynum);
