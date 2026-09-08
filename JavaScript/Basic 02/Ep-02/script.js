45def462a81eed2cd682adcbc597e8f237ed242c
Ratin <ratinhaldar123@gmail.com>
2026-09-09 00:19:40 +0530

    array part 2

---
+++ JavaScript/Basic 02/Ep-02/script.js
@@ -0,0 +1,34 @@
+// const marvelHeroes = ["Spider-Man", "Iron Man", "Hulk", "Thor", "Captain America"];
+
+// const dcHeroes = ["Batman", "Superman", "Wonder Woman", "Flash", "Aquaman"];
+
+// marvelHeroes.push("Black Panther");
+
+// console.log("marvelHeroes:", marvelHeroes.pop());
+
+// console.log("marvelHeroes:", marvelHeroes.concat(dcHeroes));
+
+// console.log(marvelHeroes);
+
+// const all_new_heros = [...marvelHeroes, ...dcHeroes]
+
+// console.log(all_new_heros);
+
+
+// const another_array = [1, 2, 3, 4, [5, 6], 7, [8, [9, 10, [11, 12]]]];
+
+// console.log(another_array);
+
+// const real_another_array = another_array.flat(Infinity);
+
+// console.log(real_another_array);
+
+// console.log(Array.from({ name: "Ratin" }))
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3));
+
+
